c10f7ac886173d7c3e351ae122b0ee2ae31c084e
Andrew Straw <strawman@astraw.com>
2009-07-24 07:48:43 -0700

    doc building script uploads to server

--- build_docs.sh
+++ build_docs.sh
@@ -8,7 +8,33 @@ VIRTUALENVDIR=PYtest
 # activate new virutal environment
 source $VIRTUALENVDIR/bin/activate
 
+# check for expected permissions on X server
+python -c 'import os; assert os.path.exists("/etc/X2.hosts")'
+python -c 'fd = open("/etc/X2.hosts",mode="r"); assert "localhost\n" in fd.readlines()'
+
+# run X server
+Xvfb :2 &
+# get PID
+XVFBPID=$!
+echo "Xvfb running in process $XVFBPID"
+
+echo "checking that Xvfb process is running"
+sleep 1
+python -c "import os; assert os.path.exists(\"/proc/$XVFBPID\")"
+echo "PID $XVFBPID seems OK, will continue with building docs"
+
+
 cd doc
 
-make clean
-make html
+set +o errexit
+DISPLAY=":2" ./clean-build-and-upload.sh
+RESULT=$?
+set -o errexit
+
+# Kill the X server
+kill $XVFBPID
+wait $XVFBPID
+
+# exit with test results
+exit $RESULT
+
